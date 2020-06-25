c5574be5c46c3da37ad2f6424a5667b74eac350f
weweyes <59646715+weweyes@users.noreply.github.com>
2020-06-25 19:02:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,12 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.8.5/g' openwrt/package/base-files/files/bin/config_generate
+# add app
+git clone https://github.com/weweyes/OpenClash.git package/lean/luci-app-openclash
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
+git clone https://github.com/littletao08/luci-app-eqos.git package/lean/luci-app-eqos
+git clone https://github.com/destan19/OpenAppFilter.git package/lean/luci-app-oaf
+git clone https://github.com/weweyes/luci-app-passwall.git package/lean/luci-app-passwall
+git clone https://github.com/weweyes/small.git package/lean/small
+# add ssrplus
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
